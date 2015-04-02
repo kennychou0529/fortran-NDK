61a0a7be23888fbf59d49f2c629f9c274ddd71d4
thomas tsai <thomas@biotrump.com>
2015-04-02 11:42:42 +0800

    !!! Importnat for cmake to define the NDK's fortran compiler.!!! Don't let cmake decide it. export FC=/home/thomas/aosp/NDK/android-ndk-r9/toolchains/arm-linux-androideabi-4.8.0/prebuilt/linux-x86/bin/arm-linux-androideabi-gfortran
    so (Fortran_COMPILER_NAME MATCHES "gfortran.*") : "-fPIE -pie -fPIC" for android
      set (CMAKE_Fortran_FLAGS_RELEASE "-funroll-all-loops -fPIE -pie -fPIC -fno-f2c -O3")
      set (CMAKE_Fortran_FLAGS_DEBUG   "-fno-f2c -O0 -fPIE -pie -fPIC -g")

--- build_android.sh
+++ build_android.sh
@@ -15,18 +15,20 @@
 #Gingerbread	2.3.3 - 2.3.7	API level 10
 #Gingerbread	2.3 - 2.3.2	API level 9, NDK 5
 #Froyo	2.2.x	API level 8, NDK 4
+
 #NDK_ROOT=${NDK_ROOT:-/home/thomas/aosp/NDK/android-ndk-r10d}
 #export NDK_ROOT
 #gofortran is supported in r9
-export NDK_ROOT=/home/thomas/aosp/NDK/android-ndk-r9
+export NDK_ROOT=${HOME}/NDK/android-ndk-r9
+export ANDROID_NDK=${NDK_ROOT}
 
 #ANDROID_APIVER=android-8
 #ANDROID_APIVER=android-9
 #android 4.0.1 ICS and above
 ANDROID_APIVER=android-14
-#TOOL="4.6"
+#TOOL_VER="4.6"
 #gfortran is in r9d V4.8.0
-TOOL="4.8.0"
+TOOL_VER="4.8.0"
 
 case $(uname -s) in
   Darwin)
@@ -69,11 +71,12 @@ esac
 
 : ${NDK_ROOT:?}
 
-echo "Using: $NDK_ROOT/toolchains/${TARGPLAT}-${TOOL}/prebuilt/${HOSTPLAT}/bin"
+echo "Using: $NDK_ROOT/toolchains/${TARGPLAT}-${TOOL_VER}/prebuilt/${HOSTPLAT}/bin"
 export ARCHI
-#export PATH="$NDK_ROOT/toolchains/${TARGPLAT}-${TOOL}/prebuilt/${HOSTPLAT}/bin/:\
-#$NDK_ROOT/toolchains/${TARGPLAT}-${TOOL}/prebuilt/${HOSTPLAT}/${TARGPLAT}/bin/:$PATH"
-export PATH="${NDK_ROOT}/toolchains/${TARGPLAT}-${TOOL}/prebuilt/${HOSTPLAT}/bin/:$PATH"
+#export PATH="$NDK_ROOT/toolchains/${TARGPLAT}-${TOOL_VER}/prebuilt/${HOSTPLAT}/bin/:\
+#$NDK_ROOT/toolchains/${TARGPLAT}-${TOOL_VER}/prebuilt/${HOSTPLAT}/${TARGPLAT}/bin/:$PATH"
+export PATH="${NDK_ROOT}/toolchains/${TARGPLAT}-${TOOL_VER}/prebuilt/${HOSTPLAT}/bin/:$PATH"
+echo $PATH
 export SYS_ROOT="${NDK_ROOT}/platforms/${ANDROID_APIVER}/arch-${ARCHI}/"
 export CC="${TARGPLAT}-gcc --sysroot=$SYS_ROOT"
 export LD="${TARGPLAT}-ld"
@@ -91,6 +94,19 @@ export FORTRAN="${TARGPLAT}-gfortran --sysroot=$SYS_ROOT"
 BLISLIB=${BLISLIB:-/home/thomas/build/libFLAME/blis/lib/armv7a/libblis.a}
 export BLISLIB
 echo $BLISLIB
-export ANDROID_NDK=${NDK_ROOT}
-cmake -DCMAKE_TOOLCHAIN_FILE=/home/thomas/build/fortran/NDK/android.toolchain.cmake -DANDROID_NDK=${NDK_ROOT} -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI="armeabi-v7a" ..
+
+#!!! quite importnat for cmake to define the NDK's fortran compiler.!!!
+#Don't let cmake decide it.
+#export FC=/home/thomas/aosp/NDK/android-ndk-r9/toolchains/arm-linux-androideabi-4.8.0/prebuilt/linux-x86/bin/arm-linux-androideabi-gfortran
+export FC=${FORTRAN}
+
+#export ANDROID_STANDALONE_TOOLCHAIN=${NDK_ROOT}/sources/cxx-stl/gnu-libstdc++/4.8
+#cmake -DCMAKE_TOOLCHAIN_FILE=/home/thomas/build/fortran/NDK/android.toolchain.cmake \
+#-DANDROID_STANDALONE_TOOLCHAIN=${NDK_ROOT}/sources/cxx-stl/gnu-libstdc++/4.8 \
+
+cmake -DCMAKE_TOOLCHAIN_FILE=../android.toolchain.cmake -DANDROID_NATIVE_API_LEVEL=${ANDROID_APIVER} \
+-DANDROID_NDK=${NDK_ROOT} -DANDROID_TOOLCHAIN_NAME=${TARGPLAT}-${TOOL_VER} \
+-DCMAKE_BUILD_TYPE=Release -DANDROID_ABI="armeabi-v7a with VFPV3" ..
+
+make -j${CORE_COUNT}
 
